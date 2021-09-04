0d81faf4405fa70183a2525b2b1ae77143ad89d1
EdTengHR <edmundteng988@gmail.com>
2021-09-04 22:28:15 +0800

    Updated visualization process handling so that stepping takes place on visualizer webserver instead of extension, added file type identification and processing, also added scaffolding for future code when sending and receiving data to visualizer and documentation

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,8 @@
 
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { readFileSync } from 'fs';
+import { stringify } from 'querystring';
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
@@ -30,36 +32,42 @@ export function activate(context: vscode.ExtensionContext) {
 
 			// Set HTML content
 			panel.webview.html = getWebviewContent('basic');
-			vscode.window
-			.showInformationMessage('VisCode Visualizer', ...['Prev', 'Next'])
-			.then(selection => {
-				console.log(selection);
-				(selection! == 'Next') ? vscode.commands.executeCommand('viscode.next') : 
-				vscode.commands.executeCommand('viscode.prev')
-			});
-		}),
+			// Read file content and convert it to string
+			var fs = require("fs");
+			var vscode = require("vscode");
+			var path = require("path");
+			var serverType;
 
-		vscode.commands.registerCommand('viscode.next', () => {
-			panel.webview.html = getWebviewContent('Next');
-			vscode.window
-			.showInformationMessage('VisCode Visualizer', ...['Prev', 'Next'])
-			.then(selection => {
-				console.log(selection);
-				(selection! == 'Next') ? vscode.commands.executeCommand('viscode.next') : 
-				vscode.commands.executeCommand('viscode.prev')
-			});
-		}),
+			// vscode.window.activeTextEditor gets editor's reference and 
+			// document.uri.fsPath returns the path to that file in string format
+			
+			// TBD - may have to deal with when types are undefined (user's active window is probably 
+			// the welcome screen or sth, if so remove '?' and handle accordingly)
+			var currentlyOpenTabfilePath = vscode.window.activeTextEditor?.document.uri.fsPath;
+			var currentlyOpenTabfileName = path.basename(vscode.window.activeTextEditor?.document.fileName);
+			const testTxt = fs.readFileSync(currentlyOpenTabfilePath).toString();
 
-		vscode.commands.registerCommand('viscode.prev', () => {
-			panel.webview.html = getWebviewContent('Prev');
-			vscode.window
-			.showInformationMessage('VisCode Visualizer', ...['Prev', 'Next'])
-			.then(selection => {
-				console.log(selection);
-				(selection! == 'Next') ? vscode.commands.executeCommand('viscode.next') : 
-				vscode.commands.executeCommand('viscode.prev')
-			});
-		})
+			// string encoding to URL encoding, to be sent to server to do trace pathing 
+			const asciiTxt = encodeURI(testTxt);
+			console.log(asciiTxt);
+			console.log('filename: ', currentlyOpenTabfileName);
+
+			// identify file type, send to corresponding server
+			serverType = 
+				(currentlyOpenTabfileName.includes('.js')) ? 'javascript' : 
+				(currentlyOpenTabfileName.includes('.py')) ? 'python' : 'unknown';
+
+			// TODO - send the asciiTxt to server by appending it in data in server url
+			if (serverType == 'unknown') 
+				console.log('text type unknown, ask user to choose which server to send to?');
+
+			// the response from the server goes into var txt, currently just a template
+			var txt = '{"code":"public class Test { public static void main(String[] args) { int x = 3; x += x; int y = 23; int z = y + x;} }","stdin":"","trace":[{"stdout":"","event":"call","line":1,"stack_to_render":[{"func_name":"main:1","encoded_locals":{},"ordered_varnames":[],"parent_frame_id_list":[],"is_highlighted":true,"is_zombie":false,"is_parent":false,"unique_hash":"1","frame_id":1}],"globals":{},"ordered_globals":[],"func_name":"main","heap":{}},{"stdout":"","event":"step_line","line":1,"stack_to_render":[{"func_name":"main:1","encoded_locals":{},"ordered_varnames":[],"parent_frame_id_list":[],"is_highlighted":true,"is_zombie":false,"is_parent":false,"unique_hash":"2","frame_id":2}],"globals":{},"ordered_globals":[],"func_name":"main","heap":{}},{"stdout":"","event":"step_line","line":1,"stack_to_render":[{"func_name":"main:1","encoded_locals":{"x":3},"ordered_varnames":["x"],"parent_frame_id_list":[],"is_highlighted":true,"is_zombie":false,"is_parent":false,"unique_hash":"4","frame_id":4}],"globals":{},"ordered_globals":[],"func_name":"main","heap":{}},{"stdout":"","event":"step_line","line":1,"stack_to_render":[{"func_name":"main:1","encoded_locals":{"x":6},"ordered_varnames":["x"],"parent_frame_id_list":[],"is_highlighted":true,"is_zombie":false,"is_parent":false,"unique_hash":"8","frame_id":8}],"globals":{},"ordered_globals":[],"func_name":"main","heap":{}},{"stdout":"","event":"step_line","line":1,"stack_to_render":[{"func_name":"main:1","encoded_locals":{"x":6,"y":23},"ordered_varnames":["x","y"],"parent_frame_id_list":[],"is_highlighted":true,"is_zombie":false,"is_parent":false,"unique_hash":"10","frame_id":10}],"globals":{},"ordered_globals":[],"func_name":"main","heap":{}},{"stdout":"","event":"step_line","line":1,"stack_to_render":[{"func_name":"main:1","encoded_locals":{"x":6,"y":23,"z":29},"ordered_varnames":["x","y","z"],"parent_frame_id_list":[],"is_highlighted":true,"is_zombie":false,"is_parent":false,"unique_hash":"14","frame_id":14}],"globals":{},"ordered_globals":[],"func_name":"main","heap":{}},{"stdout":"","event":"return","line":1,"stack_to_render":[{"func_name":"main:1","encoded_locals":{"x":6,"y":23,"z":29,"__return__":["VOID"]},"ordered_varnames":["x","y","z","__return__"],"parent_frame_id_list":[],"is_highlighted":true,"is_zombie":false,"is_parent":false,"unique_hash":"15","frame_id":15}],"globals":{},"ordered_globals":[],"func_name":"main","heap":{}}],"userlog":"Debugger VM maxMemory: 485M\n"}';
+			txt = txt.replace('\n', '');
+
+			const obj = JSON.parse(txt);
+			console.log(obj.trace[0].stack_to_render[0]);
+		}),
 	);
 	
 
@@ -76,7 +84,7 @@ export function deactivate() {
 
 function getWebviewContent(selection: string) {
 	switch (selection){
-		case 'basic' || 'Prev': {
+		case 'basic': {
 			return `
 				<!DOCTYPE html>
 					<html lang="en">
@@ -94,142 +102,11 @@ function getWebviewContent(selection: string) {
 						<title>Visualizer</title>
 					</head>
 					<body>
-						<canvas id="myCanvas" style="border:1px solid #c3c3c3;">
-						</canvas>
-						<canvas id="circle" </canvas>
-
-						<script>
-						(function() {
-							var
-							// Obtain a reference to the canvas element using its id.
-							htmlCanvas = document.getElementById('myCanvas'),
-							circleCanvas = document.getElementById('circle'),
-							// Obtain a graphics context on the canvas element for drawing.
-							context = htmlCanvas.getContext('2d');
-							circleContext = circleCanvas.getContext('2d');
-					
-							// Start listening to resize events and draw canvas.
-							initialize();
-
-							context.fillStyle = "#FF0000";
-							context.fillRect = (0, 0, window.innerWidth, window.innerHeight);
-					
-							function initialize() {
-								// Register an event listener to call the resizeCanvas() function 
-								// each time the window is resized.
-								window.addEventListener('resize', resizeCanvas, false);
-								// Draw canvas border for the first time.
-								resizeCanvas();
-							}
-					
-							// Display custom canvas. In this case it's a blue, 5 pixel 
-							// border that resizes along with the browser window.
-							function redraw() {
-								context.strokeStyle = 'blue';
-								context.lineWidth = '5';
-								context.strokeRect(0, 0, window.innerWidth, window.innerHeight);
-							
-							}
-					
-							// Runs each time the DOM window resize event fires.
-							// Resets the canvas dimensions to match window,
-							// then draws the new borders accordingly.
-							function resizeCanvas() {
-								htmlCanvas.width = window.innerWidth;
-								htmlCanvas.height = window.innerHeight;
-								circleCanvas.width = window.innerWidth;
-								circleCanvas.height = window.innerHeight;
-								redraw();
-							}
-						})();
-
-
-						</script>
+						<iframe src="https://rkds.xyz" title="description"></iframe>
 					</body>
 					</html>`;
 		}
-		case 'Next': {
-			return `
-				<!DOCTYPE html>
-					<html lang="en">
-					<head>
-						<meta charset="UTF-8">
-						<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-						<style>
-							html { height: 100%; width: 100%; padding: 0; margin: 0; }
-							body { height: 100%; width: 100%; padding: 0; margin: 0; }
-							iframe { height: 100%; width: 100%; padding: 0; margin: 0; border: 0; display: block; }
-							canvas {position: absolute; top: 0; left: 0;}
-						</style>
-
-						<title>Visualizer</title>
-					</head>
-					<body>
-						<canvas id="myCanvas" style="border:1px solid #c3c3c3;">
-						</canvas>
-						<canvas id="circle" </canvas>
-
-						<script>
-						(function() {
-							var
-							// Obtain a reference to the canvas element using its id.
-							htmlCanvas = document.getElementById('myCanvas'),
-							circleCanvas = document.getElementById('circle'),
-							// Obtain a graphics context on the canvas element for drawing.
-							context = htmlCanvas.getContext('2d');
-							circleContext = circleCanvas.getContext('2d');
-					
-							// Start listening to resize events and draw canvas.
-							initialize();
-
-							context.fillStyle = "#FF0000";
-							context.fillRect = (0, 0, window.innerWidth, window.innerHeight);
-					
-							function initialize() {
-								// Register an event listener to call the resizeCanvas() function 
-								// each time the window is resized.
-								window.addEventListener('resize', resizeCanvas, false);
-								// Draw canvas border for the first time.
-								resizeCanvas();
-							}
-					
-							// Display custom canvas. In this case it's a blue, 5 pixel 
-							// border that resizes along with the browser window.
-							function redraw() {
-								context.strokeStyle = 'blue';
-								context.lineWidth = '5';
-								context.strokeRect(0, 0, window.innerWidth, window.innerHeight);
-								
-
-								var X = circleCanvas.width/2;
-								var Y = circleCanvas.height/2;
-								var R = 45;
-								circleContext.beginPath();
-								circleContext.arc(X, Y, R, 0, 2 * Math.PI, false);
-								circleContext.lineWidth = 5;
-								circleContext.strokeStyle = '#FF0000';
-								circleContext.stroke();
-							}
-					
-							// Runs each time the DOM window resize event fires.
-							// Resets the canvas dimensions to match window,
-							// then draws the new borders accordingly.
-							function resizeCanvas() {
-								htmlCanvas.width = window.innerWidth;
-								htmlCanvas.height = window.innerHeight;
-								circleCanvas.width = window.innerWidth;
-								circleCanvas.height = window.innerHeight;
-								redraw();
-							}
-						})();
-
-
-						</script>
-					</body>
-					</html>`;
-		}
+		default: 
+			return `<html></html>`
 	}
-	return `
-	<html></html>`
 }
