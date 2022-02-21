7a432f4679f3534ff5ccfad409dc795646695f70
EdTengHR <edmundteng988@gmail.com>
2022-02-21 21:39:24 +0800

    Removed unnecessary files and testing code

--- src/utils/ManageWebviewContent.ts
+++ src/utils/ManageWebviewContent.ts
@@ -1,5 +1,4 @@
 import * as vscode from 'vscode';
-import * as d3 from 'd3';
 import * as fs from 'fs';
 
 export function getNonce() {
@@ -14,27 +13,6 @@ export function getNonce() {
 export function getWebviewContent(webview: vscode.Webview, extensionUri: vscode.Uri, 
     selection: string='response', data: string) {
 
-
-	// for testing purposes, should replace App.js file later on
-	const scriptTestPathOnDisk = vscode.Uri.joinPath(extensionUri, 'src', 'App.js');
-	// And the uri we use to load this script in the webview
-	const scriptTestUri = (scriptTestPathOnDisk).with({ 'scheme': 'vscode-resource' });
-	// Local path to css styles, replace with correct App.css later
-	const stylesPathTestPath = vscode.Uri.joinPath(extensionUri, 'src', 'App.css');
-	// Uri to load styles into webview
-	const stylesTestUri = webview.asWebviewUri(stylesPathTestPath);
-	
-	const scriptReactTestPathOnDisk = vscode.Uri.joinPath(extensionUri, 'src', 'react-app.js');
-	const scriptReactTestUri = (scriptReactTestPathOnDisk).with({ 'scheme': 'vscode-resource' });
-	const stylesPathReactTestPath = vscode.Uri.joinPath(extensionUri, 'src/css', 'geek-base.css');
-	const stylesReactTestUri = webview.asWebviewUri(stylesPathReactTestPath);
-	
-	const scriptD3TestPathOnDisk = vscode.Uri.joinPath(extensionUri, 'src', 'd3-test.js');
-	const scriptD3TestUri = (scriptD3TestPathOnDisk).with({ 'scheme': 'vscode-resource' });
-
-	const scriptD3Path = vscode.Uri.joinPath(extensionUri, 'node_modules', 'd3', 'src', 'index.js');
-	const scriptD3Uri =  webview.asWebviewUri(scriptD3Path);
-	
 	const nonce = getNonce();
 	
 	switch (selection){
@@ -58,56 +36,6 @@ export function getWebviewContent(webview: vscode.Webview, extensionUri: vscode.
 					$(window).on("load", function(e){
 						document.getElementById("myform").submit();
 					});
-				</script>
-			</body>
-			</html>
-			`
-		}
-		case 'lineHighlightTesting': {
-			return `
-			<!DOCTYPE html>
-			<html>
-			<body style="background-color:white"> 
-				<div id="StackTraceButtons" >
-					<button type="button" onclick="prevStackTraceLine()">Prev</button>
-					<button type="button" onclick="nextStackTraceLine()">Next</button>
-				</div>
-				<div id="ui" style="width:90%;height:80%;border:1px solid #000;">
-					<div id="dialog_area"><svg height="100%" width="100%" xmlns="http://www.w3.org/2000/svg"></svg></div>
-					<div id="cy"></div>
-				</div>
-				<script>
-					const vscode = acquireVsCodeApi();
-
-					var stepNum = 0
-					var lineList = {
-						"0": {
-							"line": 0,
-						},
-						"1": {
-							"line": 2,
-						},
-						"2": {
-							"line": 5,
-						},
-						"3": {
-							"line": 3,
-						},
-					}
-					function prevStackTraceLine(){
-						stepNum -= 1;
-						vscode.postMessage({
-							command: 'lineNumberChanged',
-							text: lineList[stepNum].line,
-						})
-					}
-					function nextStackTraceLine(){
-						stepNum += 1;
-						vscode.postMessage({
-							command: 'lineNumberChanged',
-							text: lineList[stepNum].line,
-						})
-					}
 				</script>
 			</body>
 			</html>
